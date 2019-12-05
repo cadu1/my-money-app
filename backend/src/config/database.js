75d3b3e3e0ecc8f0b7e9e75566ea199e808956fa
Carlos <carlos@upoutsourcing.com>
2019-12-05 15:27:29 -0300

    Update mongo lib, add 'summary' api request

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,4 +2,4 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb://localhost/mymoney')
+module.exports = mongoose.connect('mongodb://localhost/mymoney', { useNewUrlParser: true })
