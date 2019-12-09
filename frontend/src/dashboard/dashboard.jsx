8700d21fde6622d3b49294656834fcdce46ab1b5
Carlos <carlos@upoutsourcing.com>
2019-12-09 17:38:01 -0300

    Added middleware and reducer to dashboard

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import ValueBox from '../common/widget/valueBox'
+import { getSummary } from '../dashboard/dashboardActions'
 
 class Dashboard extends Component {
+  componentWillMount() {
+    this.props.getSummary()
+  }
+
   render() {
     const { credit, debit } = this.props.summary
     return (
@@ -42,5 +48,6 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = state => ({ summary: state.dashboard.summary })
+const mapDispachToProps = dispatch => bindActionCreators({ getSummary }, dispatch)
 
-export default connect(mapStateToProps)(Dashboard)
+export default connect(mapStateToProps, mapDispachToProps)(Dashboard)
