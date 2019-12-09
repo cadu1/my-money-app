64db80c110d0975e4cd23a0b32c284d22b12fb56
Carlos <carlos@upoutsourcing.com>
2019-12-09 13:08:02 -0300

    Create component menuTree

--- frontend/src/common/template/menu.jsx
+++ frontend/src/common/template/menu.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
+import MenuItem from './menuItem'
+import MenuTree from './menuTree'
+
 export default props => (
   <ul className='sidebar-menu'>
-    
+    <MenuItem
+      path='#/'
+      icon='dashboard'
+      label='Dashboard'
+    ></MenuItem>
+    <MenuTree label='Cadastro' icon='edit'>
+      <MenuItem
+        path='#billingCycle'
+        icon='usd'
+        label='Cliclos de Pagamento'
+      ></MenuItem>
+    </MenuTree>
   </ul>
 )
